474aaf6919c81d683058dbab89a28d254318b25c
PauloRLTenorio <paulorltenorio@gmail.com>
2021-11-30 02:22:07 -0300

    codigos var e conversoes

--- type-boolean.js
+++ type-boolean.js
@@ -11,4 +11,13 @@ let cadastroAtivado = true;
 const texto1 = "Alura";
 const texto2 = "alura";
 
-console.log(texto1 === texto2);
+//console.log(texto1 === texto2);
+
+// JavaScript é Case-Sensive, ou seja, diferencia maiúsculas de minúsculas
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
