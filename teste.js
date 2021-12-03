a63ef30ad8a38ad48f0c13e95a53815a67c27c96
PauloRLTenorio <paulorltenorio@gmail.com>
2021-12-03 14:17:38 -0300

    exemplos da aula 2

---
+++ teste.js
@@ -0,0 +1,17 @@
+// let usuarioConectado= false;
+// console.log (String( usuarioConectado ) ); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+// usuarioConectado= true;
+// console.log (String( usuarioConectado ) ); // agora teremos uma string “true”.
+
+
+// teremos a conversão de String para números realizado usando o + antes das variáveis
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura); 
+
+let usuarioConectado= false;
+console.log (Number( usuarioConectado ) ); 
+// teremos a conversão da booleana para número, sendo que false (falso) retorna o número 0.
+usuarioConectado= true;
+console.log (Number( usuarioConectado ) ); 
+// agora teremos a conversão de true (verdadeiro) para o número 1.
---
+++ var-let-const.js
@@ -0,0 +1,3 @@
+Unstaged changes after reset:
+D	script.js
+M	var-let-const.js
